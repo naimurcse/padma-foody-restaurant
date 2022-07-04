bccec1a141b0ea5f6adffa8439c7544d68da2dfd
naimurcse <2015naim@gmail.com>
2022-07-04 23:48:55 +0600

    FAQ Component done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import RandomFood from '../RandomFood/RandomFood';
 import SelectedFood from '../SelectedFood/SelectedFood';
 import './Cart.css';
--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -5,11 +5,11 @@ const FAQ = () => {
         <div className='faq-container'>
             <div className="faq-card">
                 <h2>How does <span style={{color:"purple"}}>React</span> work?</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint facilis dolorum earum ut molestias magni ipsa distinctio error. Consectetur exercitationem, aliquid hic accusantium excepturi culpa debitis magnam voluptates. Autem, ipsam.</p>
+                <p>React is a JavaScript library for building user interface and it's free open source library. React makes it painless to create interactive UIs. Design simple views for each state in our application, and React will efficiently update and render just the right components when our data changes. Declarative views make our code more predictable, simpler to understand, and easier to debug. Build encapsulated components that manage their state, then compose them to make complex UIs. Since component logic is written in JavaScript instead of templates, we can easily pass rich data through our app and keep the state out of the DOM.</p>
             </div>
             <div className="faq-card">
                 <h2>How does <span style={{color:"purple"}}>useState</span> work?</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint facilis dolorum earum ut molestias magni ipsa distinctio error. Consectetur exercitationem, aliquid hic accusantium excepturi culpa debitis magnam voluptates. Autem, ipsam.</p>
+                <p>The <b>useState</b> hook allows us to have state variables in the JSX functional component. It takes one argument which is the initial state and returns a state value and a function to update it.  React has two types of components, one is class components which are ES6 classes that extend from React and the other is functional components. Class components a Component and can have state and lifecycle methods: class Message extends React. The  useState hook is a special function that takes the initial state as an argument and returns an array of two entries.  UseState encapsulate only singular value from the state, for multiple state need to have useState calls.</p>
             </div>
         </div>
     );

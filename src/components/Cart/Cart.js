9164f750fafdfad5e3215e36102143f0962e7c09
naimurcse <2015naim@gmail.com>
2022-07-04 13:47:18 +0600

    choose foods css done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,14 +5,14 @@ import './Cart.css';
 const Cart = ({cart}) => {
     console.log(cart);  
     return (
-        <div>
+        <div className='cart-component'>
             <h2>Selected Food </h2>
             {
                 cart.map(food=><SelectedFood food={food}></SelectedFood>)
             }
             <div className="button-container">
-                <button className='btn' style={{padding:"16px 35px"}}>Select Random 1</button>
-                <button className='btn'>Reset The Cart</button>
+                <button className='btn' style={{padding:"16px 35px", backgroundColor:"green",color:"#fff"}}>Select Random 1</button>
+                <button className='btn' style={{backgroundColor:"tomato",color:"#fff"}}>Reset The Cart</button>
             </div>
         </div>
     );
